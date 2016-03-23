8c497a92822fb4b451781155f65e6af81206f46d
Tobias Müller <t.mueller@i-digital-m.com>
2016-03-23 09:25:33 +0100

    initial

---
+++ src/main/java/test/OutTest.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author tobias
+ *
+ */
+public class OutTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
